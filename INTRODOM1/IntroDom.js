ceefb5ff2bff57603065ce3310842b36fc6b4abe
Nannita1403 <nannimagi@gmail.com>
2024-02-09 22:21:22 +0100

    subida

--- INTRODOM1/IntroDom.js
+++ INTRODOM1/IntroDom.js
@@ -38,8 +38,6 @@ console.log(title);
 console.log(title2);
 console.log(title3);
 
-
-
 //? METODO PARA ELEMENTOS PADRES E HIJOS
 //* 1) METODO PARA VER QUE ETIQUETA ESTA ENCIMA (PADRE DIRECTO) - NO
 console.log(title.parentElement);
@@ -49,5 +47,62 @@ console.log(title.parentElement);
 const div = document.querySelector("#paraghaphdiv")
 console.log(div.childNodes);
 
+console.log("----------------------------------");
 
+//? COmo hacer para insertar contenido desde JS a HTMl (no solo traer)
+// crear un nodo desde cero.
+
+//* 1) insertar una etiqueta con texto:
+
+const title4 = document.createElement ("h1"); //aca no esta reflejado todavia (esta en el limbo)
+title4.innerText = "Title";
+console.log(title4);
+
+// aca lo que voy a hacer es insertarlo en el Docuemnto como un nuevo hijo
+document.body.appendChild(title4);
+
+//* 2) Imagen, src con una imagen y cargarla:
+
+const imagen = document.createElement("img");
+// traigo la direccion de la imagen que quiero
+imagen.src = "https://tse1.mm.bing.net/th?id=OIP.Hf1fzVhQli-QEEdHhyDydwHaIr&pid=Api&P=0&h=180";
+// pongo un tecto alternativo:
+imagen.alt = "JavaScript logo"
+// cargo la imagen en HTML:
+document.body.appendChild(imagen);
+
+
+//* 3) TAMBIEN EXISTE UNA MANERA RAPIDA PERO PERDEMOS USOS DE METODOS DEL NODO
+// vamos a hacer la comparativa:
+
+const div2 = document.createElement("div");
+// Aca a difentencia insertamso directamente en el HTML el textp
+div2.innerHTML = `<p>Soy un segundo div<p>`;
+document.body,appendChild(div2)
+
+//* 4) Tambien podemos crear Templates o plantillas:
+//! muy importante por que se pueden reutilizar:
+const div3 = document.createElement("div");
+// aca agregamos el contenido del texto:
+const content = "Este es el contenido" 
+// aca no hay contenido (como antes) sino la VARIABLE que creamos en la linea anterior
+div3.innerHTML = `<p>${content}<p>`; 
+document.body.appendChild(div3); 
+
+//? AHORA A TRABAJAR CON CLASES (STILOS DE CSS)
+// propiedad: class list y podemos acceder para agregar, modificar, y comprobar las clases 
+
+image.classList.add("byw"); //Me permite AGREGAR una clase del CSS Y Aplicarla A HTML
+image.classList.remove("byw"); // Me permite ELIMINAR una Clase aplicada en el HTML de estilo
+
+// Toggle se usa para poder armar un estilo tipo ON OFF que me permite por ej:
+// 1) ampliar y reducir un menu desplegable cuando presiono
+// 2) Ampliar o reducir un imagen al tocarla 
+//? toggle tiene algo muy bueno que es: que si no existe la clase la añado pero si la encuentra te la quito
+image.classList.toggle("byw"); //aqui la aplica por que no la encuentra //!ON - true
+image.classList.toggle("byw"); // aqui la elimina por que ya estaba aplicada //!OFF - false
+image.classList.toggle("byw");
+
+// Se puede verificar si tiene la clase aplicada con: 
+console.log(image.classList.contains("byw")); //Aca va a dar un true o false.
 
--- INTRODOM1/fichero.js
+++ INTRODOM1/fichero.js
@@ -16,7 +16,7 @@ const allDivP = document.querySelectorAll("div > p")
 
 console.log(allDivP);
 
-//? METODO PARA ELEMENTOS PADRES E HIJOS
+//? METODO PARA VER ELEMENTOS PADRES E HIJOS
 //* 1) METODO PARA VER QUE ETIQUETA ESTA ENCIMA (PADRE DIRECTO) - NO
 console.log(title.parentElement);
 
@@ -24,3 +24,52 @@ console.log(title.parentElement);
 // ACA SACAMOS CON ID = USANDO # + el ID de HTML para ver los hijos y el contenido
 const div = document.querySelector("#paraghaphdiv")
 console.log(div.childNodes);
+
+//? METODO PARA CREAR ELEMENTOS 
+//? 1) crear elementos para inyectar elementos en JS para insertar en HTML
+// crear un nodo desde cero.
+//* 1) crear un Tiulo= h1 + nodo desde cero
+const title4 = document.createElement ("h1"); //aca no esta reflejado todavia (esta en el limbo)
+title4.innerText = "Title";
+console.log(title4);
+//* 2) aca lo que voy a hacer es insertarlo en el Docuemnto como un nuevo hijo
+document.body.appendChild(title4);
+
+//? 2) crear y cargar una imagen:
+
+const image = document.createElement("img");
+// traigo la direccion de la imagen que quiero
+image.src = "https://tse1.mm.bing.net/th?id=OIP.Hf1fzVhQli-QEEdHhyDydwHaIr&pid=Api&P=0&h=180";
+// pongo un tecto alternativo:
+image.alt = "JavaScript logo"
+
+document.body.appendChild(image);
+
+//? 3) TAMBIEN EXISTE UNA MANERA RAPIDA PERO PERDEMOS PODERES DEL NODO
+const div2 = document.createElement("div");
+
+div2.innerHTML = `<p>Soy un segundo div<p>`;
+document.body.appendChild(div2); // esto funciona solo por que DIV es un nodo
+
+//? 4) Tambien podemos crear Templates o plantillas:
+const div3 = document.createElement("div");
+// aca agregamos el contenido del texto:
+const content = "Este es el contenido" 
+// aca no hay contenido (como antes) sino la VARIABLE que creamos en la linea anterior
+div3.innerHTML = `<p>${content}<p>`; 
+document.body.appendChild(div3); 
+
+//? AHORA A TRABAJAR CON CLASES (STILOS DE CSS)
+// propiedad: class list y podemos acceder para agregar, modificar, y comprobar las clases 
+
+image.classList.add("byw"); //Me permite AGREGAR una clase del CSS Y Aplicarla A HTML
+image.classList.remove("byw"); // Me permite ELIMINAR una Clase aplicada en el HTML de estilo
+
+//? toggle: añade estilo si no esta aplicado pero si la encuentra te la quito
+image.classList.toggle("byw"); //aqui la aplica por que no la encuentra //!ON - true
+image.classList.toggle("byw"); // aqui la elimina por que ya estaba aplicada //!OFF - false
+image.classList.toggle("byw");
+
+// Se puede verificar si tiene la clase aplicada con: 
+console.log(image.classList.contains("byw")); //Aca va a dar un true o false.
+
