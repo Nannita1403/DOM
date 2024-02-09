a0e6d5213a556076f27934c4d0755dcd0a5c8fa1
Nannita1403 <nannimagi@gmail.com>
2024-02-09 15:24:01 +0100

    ok

---
+++ INTRODOM1/IntroDom.js
@@ -0,0 +1,21 @@
+//* Video de INTRO DOM 1
+// como usar js en html: 
+
+//! 1ra manera:
+//? crear una etiquera al final de Body (por que se leera cuando termina el contenido de HTML)ç
+// <scrip src="nombre del fichero.js"></scrip>
+
+//! 2da manera:
+//? puede ser mas limpio o ordenado. ADEMAS que no le pongamos algo debajo
+//? LO PONEMOS EN EL HEAD - como cuando linqueo un css, pero:
+<scrip src="nombre del fichero.js" defer ></scrip>
+// el defer lo que hace es como un stop. Le indica que espere que el codigo se renderice Primero
+//para luego ejecutar el archivo JS liqueado. Ppor que el codigo se lee de arriba hacia abajo
+// y sino esto generaria un problema.
+
+//? METODO PARA RECUPERAR LO QUE HAY EN EL HTML Y PODER TRABAJAR
+//VAMOS A RECUPERAR EL TITULO
+
+
+
+
