23cacde389ca0a5d3ce6954162ea4212a97c5d86
Nannita1403 <nannimagi@gmail.com>
2024-02-14 11:09:47 +0100

    oki

---
+++ ej_14_02/contar_palabras.js
@@ -0,0 +1,10 @@
+const frase=
+"Hoy me fui al supermercado a comprar el nuevo turron de David Muñoz";
+
+const arrayPalabras = frase.split(" ");
+
+for (const palabra of arrayPalabras) {
+console.log(palabra + " tiene " + palabra.length + " longitud.");
+}
+    
+
