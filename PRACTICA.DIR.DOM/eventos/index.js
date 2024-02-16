af9ccc2acf92a2717bb73bc504c7fa88b65ba0b8
Nannita1403 <nannimagi@gmail.com>
2024-02-16 14:38:11 +0100

    FIN

--- PRACTICA.DIR.DOM/eventos/index.js
+++ PRACTICA.DIR.DOM/eventos/index.js
@@ -1,48 +1,56 @@
+//? se toma el boton por que es lo que siempre intentamos hacer es lo tipico
 //! lo primero que necesito para darle funcionalidad al botón es el botón
-const button = document.querySelector("button");
+const button = document.querySelector("button"); // aca tengo uno solo entonces olo tomo por etiqueta
 
-function darVuelta (e) {
+
+function darVuelta (e) { // creo la funcion para que pasa cuando pasamos el mouse por arriba del boton
     /* console.log(event.target.remove()); */
+    //* TARGET= PARRAFO DONDE ESTOY EJECUTANDO EL EVENTO.ELIMINAR
     e.target.style.transform = "rotate(180deg)";
 }
-
-/* function addEventos () {
-    const allP = document.querySelectorAll("p");
-    allP[0].addEventListener("mouseover", (e) => darVuelta(e));
-} */
-
 let cantidadParrafos = 0;
 
 function crearParrafo () {
     const p = document.createElement("p");
 
-    cantidadParrafos++;
+    cantidadParrafos++; //ACA SUMO PARRAFO UNO EN UNO
 
     p.textContent = "Soy un párrafo nuevo";
 
-    if (cantidadParrafos === 1) {
+ 
+    if (cantidadParrafos === 1) {     // ESTO ES PARA QUE SOLO SE DE VUELTA EL PRIMER PARRAFO
         p.addEventListener("mouseover", (e) => darVuelta(e));
         /* p.addEventListener("mouseover", darVuelta); */
     }
 
+    //! manera diferente de tomar solo el primer parrafo:
+/* function addEventos () {
+    const allP = document.querySelectorAll("p"); //* tomo todos los parrafor
+    allP[0].addEventListener("mouseover", (e) => darVuelta(e));
+} */
     document.body.appendChild(p);
-    /* addEventos(); */
-}
+    /* addEventos(); */ //! ultima parte de la segunda manera de marcar solo el 1er parrafo
+    //* ojo ACA siempre el evento se tiene que llamar despues de crear los parrafos a HTML
+} 
 
+button.addEventListener("click", () => crearParrafo("Soy un párrafo nuevo"))
+
+
+
+
+
+
+
+//? 1ra manera de actuar con el BOTON
 /* button.onclick = () => crearParrafo("Soy un párrafo nuevo"); */
 
-/* button.addEventListener("click", nombreFuncion);
-
+//? 2da manera de actuar con el BOTON
+/* button.addEventListener("click", crearParrafo);
 
 function nombreFuncion () {
     crearParrafo("Soy un párrafo nuevo");
 }
+//* = a  button.addEventListener("click", () => crearParrafo("Soy un párrafo nuevo"))
+ */
 
-
-button.addEventListener("click", () => crearParrafo("Soy un párrafo nuevo")); */
-
-button.addEventListener("click", crearParrafo);
-
-
-
-//! contras - cuando estamos usando programación orientada a objetos POO
+//! contras de Arrow Functions- cuando estamos usando programación orientada a objetos POO
--- PRACTICA.DIR.DOM/practica/index.js
+++ PRACTICA.DIR.DOM/practica/index.js
@@ -1,26 +1,52 @@
 const emojis = ["😂", "😁", "😎", "😍", "😴", "🤐"];
 const numbers = [32442, 234, 2, 34, 234, 23, 4, 23, 423, 4, 23, 4];
 
+//! PRIMERO MALA MANERA = NO SE HACE ASI:
+//? para la lista de los emojis
+
+//* selecciono el contenedor donde voy a poner los emojis
+/* const emojiDiv = document.querySelector(".insert-emoji-here");
+
+//* recorro el array de emojis para llegar a ellos de 1 en 1
+for (const emoji of emojis) {  ///recorro el array de emojis para llegar a ellos de 1 en 1
+    
+    const pEmoji = document.createElement("p");    //* creo un elemento de parrafo
+    
+    pEmoji.textContent = emoji;                    //* Le agrego el parrafo el valor: emoji
+    emojiDiv.appendChild(pEmoji);                  //* meto el emoji en el contenedor
+}*/
+//? para la lista los numeros
+/* const numDiv = document.querySelector(".insert-num-here");
+
+for (const number of numbers) {  ///recorro el array de emojis para llegar a ellos de 1 en 1
+    
+    const pNumber = document.createElement("p");    // creo un elemento de parrafo
+    
+    pNumber.textContent = number;                    // Le agrego el texto = emoji
+    numDiv.appendChild(pNumber);                  // y lo meto en el HTML
+} */
+
+//! AHORA DE LA MANERA QUE CORRESPONDE: con Arrow function:
+//* para lo que no puedo cambiar que no es GENERICO, me llega por parametro a la función
 //! declaración
+            //(lo que no puedo cambiar y meto pr parametro) //* elemento padre= numeros o emojis y tmb el ARRAY
 const printDivs = (selectorPadre, array) => {
     const div = document.querySelector(selectorPadre);
 
-    // limpiar el contenedor
+    // limpiar el div para cuando agregue el nuevo nro o emoji no se repitan sino qu agregue de a 1
     div.innerHTML = ``;
 
     for (const element of array) {
 
         const p = document.createElement("p");
-
         p.textContent = element;
-
+        // aca agrego un llamado cuadno usuario hace click en los elementos que ya estan en la caja
         p.addEventListener("click", (e) => e.target.remove());
-
         div.appendChild(p);
     }
 }
 
-//! ejecuciones
+//! ejecuciones ACA LES ENVIO QUIEN ES: EL SELECTOR PADRE Y EL ARRAY!!!
 printDivs(".insert-num-here", numbers);
 printDivs(".insert-emoji-here", emojis);
 
@@ -34,21 +60,30 @@ const button = document.querySelector("button");
 
 const insertarDato = () => {
     //! saber que ha escrito el usuario
+// NaN es: Not an number || parseInt = pasado a numerico||  input.vale = me permite sabes que ha escrito como valor 
 
     if (isNaN(parseInt(input.value)) === true) {
-        // el código si el dato NO es un número
-        console.log(input.value);
+        console.log(input.value); // sale el emoji en consola
+        //? el código si el dato NO es un número lo introducimos en los emojis
         emojis.push(input.value);
-        printDivs(".insert-emoji-here", emojis);
+        printDivs(".insert-emoji-here", emojis); // ACA LO METO EN EL DOM por que sino mas alla que se agreguen solo se ven en JS
     } else {
-        // el código si el dato es un número
+        //? el código si el dato SI ES un número lo introducimos en los numeros
         numbers.push(parseInt(input.value));
-        printDivs(".insert-num-here", numbers);
+        printDivs(".insert-num-here", numbers); // ACA LO METO EN EL DOM por que sino mas alla que se agreguen solo se ven en JS
     }
-
+    //? aca limpiamos el espacio del imput para volver a cargar de nuevo
     input.value = "";
 
 }
 
 //! controlar cuando quiere enviar el usuario el nuevo dato - "cuando haga click el botón"
+/* button.onclick = () => alert("he hecho el click");*/ //* esto me permite ir viendo si funciona con un alerta y un arrow funcion
 button.onclick = insertarDato;
+
+
+
+
+//! EJERCITACION CON SPLICE:
+// QUE CUANDO YO QUITE ALGO DEL DIV, ESO NO VUELVA APARECER DEL ARRAY INICIAL
+
